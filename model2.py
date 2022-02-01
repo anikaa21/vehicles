ce3214e2743e71ed788851f14251f824d1532bff
anikaa21 <97326042+anikaa21@users.noreply.github.com>
2022-02-01 11:55:17 +0530

    Update model2.py

--- model2.py
+++ model2.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
+import xgboost as xgb
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
 df=pd.read_csv('Real_Combine.csv')
 df.isnull().sum()
 df=df.dropna()
@@ -10,27 +15,18 @@ df=df.dropna()
 Xx=df.iloc[:,:-1] ## independent features
 yy=df.iloc[:,-1] 
 
-from sklearn.ensemble import ExtraTreesRegressor
-import matplotlib.pyplot as plt
-model = ExtraTreesRegressor()
-model.fit(Xx,yy)
-from sklearn.model_selection import train_test_split
 Xx_train, Xx_test, yy_train, yy_test = train_test_split(Xx, yy, test_size=0.3, random_state=0)
-
-import xgboost as xgb
 
 regressor=xgb.XGBRegressor()
 regressor.fit(Xx_train,yy_train)
 
 regressor.score(Xx_train,yy_train)
-from sklearn.model_selection import cross_val_score
+
 score=cross_val_score(regressor,Xx,yy,cv=5)
 #Evaluating The Model
 prediction=regressor.predict(Xx_test)
-import numpy as np
 Xx_train=np.array(Xx_train)
 
-from sklearn.model_selection import RandomizedSearchCV
 
 n_estimators = [int(x) for x in np.linspace(start = 100, stop = 1200, num = 12)]
 learning_rate = ['0.05','0.1', '0.2','0.3','0.5','0.6']
@@ -52,3 +48,6 @@ xg_random.fit(Xx_train,yy_train)
 file = open('xgboost_random_model.pkl', 'wb')
 
 pickle.dump(xg_random, file)
+
+pickle.dump(xg_random,open('xgboost_random_model.pkl','wb'))
+model=pickle.load(open('xgboost_random_model.pkl','rb'))
